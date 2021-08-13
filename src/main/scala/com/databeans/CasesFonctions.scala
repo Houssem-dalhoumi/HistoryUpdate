ba2257d49b946cf7000bb5f111f7f2e26cddcf57
Houssem-dalhoumi <dalhoumihoussem3@gmail.com>
2021-08-13 12:35:23 +0200

    CasesFonctions tested

--- src/main/scala/com/databeans/CasesFonctions.scala
+++ src/main/scala/com/databeans/CasesFonctions.scala
@@ -9,20 +9,55 @@ object CasesFonctions {
   def addressHistoryUpdate (addressHistoryInput: DataFrame, addressUpdateInput: DataFrame): DataFrame = {
     val renamedDataframe = addressUpdateInput.withColumnRenamed("address","new_address")
       .withColumnRenamed("moved_in","new_moved_in").select("id","new_address","new_moved_in")
-    val joinedDataframe = addressHistoryInput.join(renamedDataframe, "id")
-    val firstRow = joinedDataframe
+    val shouldBeUpdated = addressHistoryInput.where(col("current") === true)
+    val joinedDataframe = shouldBeUpdated.join(renamedDataframe, "id")
+
+    val diffrentAddressDiffrentTime = joinedDataframe.where(col("address" ) =!= col("new_address"))
+      .where(col("new_moved_in") > col("moved_in"))
+    val firstRow = diffrentAddressDiffrentTime
       .withColumn("moved_out",col("new_moved_in"))
       .withColumn("current",lit(false))
       .drop("new_address","new_moved_in")
-    val secondRow = joinedDataframe
+    val secondRow = diffrentAddressDiffrentTime
       .withColumn("address",col("new_address"))
       .withColumn("moved_in",col("new_moved_in"))
       .drop("new_address","new_moved_in")
     val oldRow = joinedDataframe
       .drop("new_address", "new_moved_in")
 
-    val updatedDataframe = firstRow.union(secondRow).union(addressHistoryInput).except(oldRow)
-    updatedDataframe
+    val diffrentAddressDiffrentTime2 = joinedDataframe.where(col("new_moved_in") < col("moved_in"))
+      .where(col("new_address") =!= col("address"))
+    val firstRow3 = diffrentAddressDiffrentTime2
+      .withColumn("address", col("new_address"))
+      .withColumn("moved_out", col("moved_in"))
+      .withColumn("moved_in", col("new_moved_in"))
+      .withColumn("current", lit(false))
+      .drop("new_address", "new_moved_in")
+    val secondRow3 = diffrentAddressDiffrentTime2
+      .drop("new_address", "new_moved_in")
+
+    val sameAddressDiffrentTime = joinedDataframe.where(col("new_moved_in" ) < col("moved_in"))
+      .where(col("address") === col("new_address"))
+    val firstRow1 = sameAddressDiffrentTime
+      .withColumn("moved_in", col("new_moved_in"))
+      .drop("new_address","new_moved_in")
+
+    val sameAddressDiffrentTime2 = joinedDataframe.where(col("new_moved_in" ) > col("moved_in"))
+      .where(col("address") === col("new_address"))
+    val secondRow4 = sameAddressDiffrentTime2
+      .drop("new_address", "new_moved_in")
+
+
+    val newPerson = addressUpdateInput.join(shouldBeUpdated, shouldBeUpdated("id") === renamedDataframe("id") , "left_anti")
+    val newPersonAdd = newPerson.withColumn("moved_out", lit(null))
+      .withColumn("current" , lit(true))
+
+
+    val updatedDataframe = firstRow.union(secondRow).union(firstRow1).union(addressHistoryInput).except(oldRow).union(firstRow3).union(newPersonAdd)
+    val finalDataframe = updatedDataframe.union(secondRow3).union(secondRow4)
+
+    finalDataframe
+
   }
 
 }
--- src/test/scala/com/databeans/CasesFonctionsSpec.scala
+++ src/test/scala/com/databeans/CasesFonctionsSpec.scala
@@ -25,24 +25,136 @@ class CasesFonctionsSpec extends AnyFlatSpec with Matchers with GivenWhenThen {
 
   val pattern = DateTimeFormatter.ofPattern("dd-MM-yyyy")
 
-  val addressHistory = Seq(AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), null, true),
-    AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-02-2020", pattern), null, true)
+  val addressHistory = Seq(
+    AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), null, true),
+    AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-02-2020", pattern), null, true),
+    AddressHistory(3, "banneni", "oussema", "paris", LocalDate.parse("05-06-2019", pattern), null, true),
+    AddressHistory(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-12-1992", pattern), null, true),
+    AddressHistory(2, "abidi", "jacer", "kasserine", LocalDate.parse("01-02-1992", pattern), LocalDate.parse("01-02-2020", pattern), false)
   ).toDF()
-  val addressUpdate = Seq(AddressUpdates(1, "Boazizi", "Sayf", "Kasserine", LocalDate.parse("01-09-2020", pattern))).toDF()
-  val expectedResult = Seq(AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), LocalDate.parse("01-09-2020", pattern), false),
-                           AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-02-2020", pattern), null, true),
-                           AddressHistory(1, "Boazizi", "Sayf", "Kasserine", LocalDate.parse("01-09-2020", pattern), null, true)
+  val addressUpdate = Seq(
+    AddressUpdates(1, "Boazizi", "Sayf", "Kasserine", LocalDate.parse("01-09-2020", pattern)),
+    AddressUpdates(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-01-2020", pattern)),
+    AddressUpdates(3, "banneni", "oussema", "kasserine", LocalDate.parse("01-08-1992", pattern)),
+    AddressUpdates(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-08-2002", pattern)),
+    AddressUpdates(5, "abidi", "ghassen", "tatawin", LocalDate.parse("01-08-2020", pattern))
+
+
   ).toDF()
 
-  "AddressHistoryBuilder" should "update the address history when given an update" in {
+
+
+  val addressUpdate1 = Seq(AddressUpdates(1, "Boazizi", "Sayf", "Kasserine", LocalDate.parse("01-09-2020", pattern))).toDF()
+
+  "AddressHistoryBuilder" should "update the history when given a diffrent address and a diffrent time update " in {
     Given("the address history and the update")
     val addressHistoryInput = addressHistory
-    val addressUpdateInput = addressUpdate
+    val addressUpdateInput = addressUpdate1
     When("AddressHistoryUpdate is Invoked")
-    val dataframeUpdater = CasesFonctions.addressHistoryUpdate(addressHistoryInput, addressUpdateInput)
+    val dataframeUpdater1 = CasesFonctions.addressHistoryUpdate(addressHistoryInput, addressUpdateInput)
     Then("the address history should be updated")
-    expectedResult.collect() should contain theSameElementsAs(dataframeUpdater.collect())
+    val expectedResult1 = Seq(
+      AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), LocalDate.parse("01-09-2020", pattern), false),
+      AddressHistory(1, "Boazizi", "Sayf", "Kasserine", LocalDate.parse("01-09-2020", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-02-2020", pattern), null, true),
+      AddressHistory(3, "banneni", "oussema", "paris", LocalDate.parse("05-06-2019", pattern), null, true),
+      AddressHistory(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-12-1992", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "kasserine", LocalDate.parse("01-02-1992", pattern), LocalDate.parse("01-02-2020", pattern), false)
+    ).toDF()
 
+    expectedResult1.collect() should contain theSameElementsAs (dataframeUpdater1.collect())
+
+
+  }
+
+  val addressUpdate2 = Seq(AddressUpdates(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-01-2020", pattern))).toDF()
+
+  "AddressHistoryBuilder" should "update the history when given a same address and a diffrent time update " in {
+    Given("the address history and the update")
+    val addressHistoryInput = addressHistory
+    val addressUpdateInput = addressUpdate2
+
+    When("AddressHistoryUpdate is Invoked")
+    val dataframeUpdater2 = CasesFonctions.addressHistoryUpdate(addressHistoryInput, addressUpdateInput)
+
+    Then("the address history should be updated")
+    val expectedResult2 = Seq(
+      AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-01-2020", pattern), null, true),
+      AddressHistory(3, "banneni", "oussema", "paris", LocalDate.parse("05-06-2019", pattern), null, true),
+      AddressHistory(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-12-1992", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "kasserine", LocalDate.parse("01-02-1992", pattern), LocalDate.parse("01-02-2020", pattern), false)
+    ).toDF()
+
+    expectedResult2.collect() should contain theSameElementsAs (dataframeUpdater2.collect())
+  }
+
+
+  val addressUpdate3 = Seq(AddressUpdates(3, "banneni", "oussema", "kasserine", LocalDate.parse("01-08-1992", pattern))).toDF()
+
+  "AddressHistoryBuilder" should "update the history when given a diffrent address and a diffrent time2  update " in {
+    Given("the address history and the update")
+    val addressHistoryInput = addressHistory
+    val addressUpdateInput = addressUpdate3
+
+    When("AddressHistoryUpdate is Invoked")
+    val dataframeUpdater3 = CasesFonctions.addressHistoryUpdate(addressHistoryInput, addressUpdateInput)
+
+    Then("the address history should be updated")
+    val expectedResult3 =Seq(
+      AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-02-2020", pattern), null, true),
+      AddressHistory(3, "banneni", "oussema", "paris", LocalDate.parse("05-06-2019", pattern), null, true),
+      AddressHistory(3, "banneni", "oussema", "kasserine", LocalDate.parse("01-08-1992", pattern), LocalDate.parse("05-06-2019", pattern), false),
+      AddressHistory(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-12-1992", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "kasserine", LocalDate.parse("01-02-1992", pattern), LocalDate.parse("01-02-2020", pattern), false)
+    ).toDF()
+
+    expectedResult3.collect() should contain theSameElementsAs (dataframeUpdater3.collect())
+  }
+
+
+  val addressUpdate4 = Seq( AddressUpdates(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-08-2002", pattern))).toDF()
+  "AddressHistoryBuilder" should "update the history when given a same address and a diffrent time2  update " in {
+    Given("the address history and the update")
+    val addressHistoryInput = addressHistory
+    val addressUpdateInput = addressUpdate4
+
+    When("AddressHistoryUpdate is Invoked")
+    val dataframeUpdater4 = CasesFonctions.addressHistoryUpdate(addressHistoryInput, addressUpdateInput)
+
+    Then("the address history should be updated")
+    val expectedResult4 = Seq(
+      AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-02-2020", pattern), null, true),
+      AddressHistory(3, "banneni", "oussema", "paris", LocalDate.parse("05-06-2019", pattern), null, true),
+      AddressHistory(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-12-1992", pattern), null, true),
+      AddressHistory(2, "abidi", "jacer", "kasserine", LocalDate.parse("01-02-1992", pattern), LocalDate.parse("01-02-2020", pattern), false)
+    ).toDF()
+    expectedResult4.collect() should contain theSameElementsAs (dataframeUpdater4.collect())
+
+  }
+
+  val addressUpdate5 = Seq(AddressUpdates(5, "abidi", "ghassen", "tatawin", LocalDate.parse("01-08-2020", pattern))).toDF()
+  "AddressHistoryBuilder" should "update the history when given a new person " in {
+    Given("the address history and the update")
+    val addressHistoryInput = addressHistory
+    val addressUpdateInput = addressUpdate5
+
+    When("AddressHistoryUpdate is Invoked")
+    val dataframeUpdater5 = CasesFonctions.addressHistoryUpdate(addressHistoryInput, addressUpdateInput)
+
+    Then("the address history should be updated")
+     val expectedResult5 = Seq(
+       AddressHistory(1, "Boazizi", "Sayf", "Soussa", LocalDate.parse("01-06-2020", pattern), null, true),
+       AddressHistory(2, "abidi", "jacer", "amesterdam", LocalDate.parse("01-02-2020", pattern), null, true),
+       AddressHistory(3, "banneni", "oussema", "paris", LocalDate.parse("05-06-2019", pattern), null, true),
+       AddressHistory(4, "dalhoumi", "houssem", "kasserine", LocalDate.parse("01-12-1992", pattern), null, true),
+       AddressHistory(5, "abidi", "ghassen", "tatawin", LocalDate.parse("01-08-2020", pattern), null,true),
+       AddressHistory(2, "abidi", "jacer", "kasserine", LocalDate.parse("01-02-1992", pattern), LocalDate.parse("01-02-2020", pattern), false)
+     ).toDF()
+
+    expectedResult5.collect() should contain theSameElementsAs (dataframeUpdater5.collect())
 
   }
 
